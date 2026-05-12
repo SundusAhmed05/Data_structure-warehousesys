40f2e6ed2ac4f12f925bca43669067e911bc3d4f
Sundus Ahmed Mahamoud <sundusahmed434@gmail.com>
2026-05-12 12:27:23 +0300

    Refactor palindrome check using a Stack structure

--- palindromes.cpp
+++ palindromes.cpp
@@ -2,34 +2,41 @@
 #include <string>
 using namespace std;
 
+struct Stack {
+    char data[100];
+    int top;
+};
+
 int main() {
 
     string word;
     string reversed = "";
 
-    char stack[100];
-    int top = -1;
+    Stack stack;
+    stack.top = -1;
 
     cout << "Enter a word: ";
     cin >> word;
 
-    // Push characters into stack
+    // PUSH
     for (int i = 0; i < word.length(); i++) {
-        top++;
-        stack[top] = word[i];
+        stack.top++;
+        stack.data[stack.top] = word[i];
     }
 
-    // Pop characters from stack
-    while (top != -1) {
-        reversed += stack[top];
-        top--;
+    // POP
+    while (stack.top != -1) {
+        reversed += stack.data[stack.top];
+        stack.top--;
     }
 
-    // Check palindrome
+    // CHECK PALINDROME
     if (word == reversed) {
         cout << "It is a Palindrome." << endl;
-    } else {
+    }
+    else {
         cout << "It is NOT a Palindrome." << endl;
     }
-return 0;
+
+    return 0;
 }
